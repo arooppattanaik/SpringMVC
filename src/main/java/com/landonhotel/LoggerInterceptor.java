de0c2bda68ffc4d65a83190d98f1c651221b073e
arooppattanaik <arooppattanaik@gmail.com>
2019-01-02 00:47:21 -0500

    AP: Spring MVC and navigate using request mapping

---
+++ src/main/java/com/landonhotel/LoggerInterceptor.java
@@ -0,0 +1,7 @@
+package com.landonhotel;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoggerInterceptor  extends HandlerInterceptorAdapter {
+	// customize logging levels here: debug, error, info, fatal
+}
